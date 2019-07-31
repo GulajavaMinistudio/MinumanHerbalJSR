fb7c4d05715116efc867476990a701b117e5395a
GulajavaMinistudio <gulajava.mini@gmail.com>
2019-07-31 07:55:04 +0700

    Pembuatan daftar menu minuman

--- src/app/list-minuman/list-resep-minuman/list-resep-minuman.component.ts
+++ src/app/list-minuman/list-resep-minuman/list-resep-minuman.component.ts
@@ -1,25 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataLoadersService } from 'src/app/services/data-loaders.service';
+import { Subscription } from 'rxjs';
+import { ResepModel } from 'src/app/models/ResepModel';
 
 @Component({
   selector: 'app-list-resep-minuman',
   templateUrl: './list-resep-minuman.component.html',
   styleUrls: ['./list-resep-minuman.component.css']
 })
-export class ListResepMinumanComponent implements OnInit {
+export class ListResepMinumanComponent implements OnInit, OnDestroy {
+
 
   listDaftarResep: string[] = [];
+  subscribption: Subscription = new Subscription();
 
   constructor(private readonly https: DataLoadersService) { }
 
   ngOnInit() {
+    this.subscribption = new Subscription();
+    this.getDaftarResep();
   }
 
   getDaftarResep() {
 
+    const subs = this.https.getDataResepMinuman()
+    .subscribe(
+      (result: ResepModel) => {
+        this.listDaftarResep = result.listResepMinuman;
+      },
+      (errors) => {
+        console.log(errors);
+      }
+    );
+
+    this.subscribption.add(subs);
   }
 
-  onClickResepMakanan(index: number) {
+  onClickResepMakanan(index: number, item: string) {
 
+  }
+
+  ngOnDestroy(): void {
+
+    this.subscribption.unsubscribe();
   }
 }
